0a5b14b88990f47f80cd9cb968c874f057a1bb47
talibhmukadam <talibhmukadam@gmail.com>
2025-05-22 17:49:55 +0700

    Update script.py

--- script.py
+++ script.py
@@ -11,12 +11,17 @@ with SB(uc=True, test=True, locale="en") as sb:
     # sb.cdp.gui_click_element('button[aria-haspopup="true"]')
     sb.cdp.find_element('[id="file-upload"]', best_match=False, timeout=10).send_file('/Users/runner/app/resume.pdf')
     sb.sleep(10)
+    sb.cdp.save_screenshot('abc1.png', folder=None, selector=None)
     sb.cdp.find_element('[data-ui="lastname"]', best_match=False, timeout=10).send_keys('Narsingarh wala')
     sb.sleep(5)
     sb.cdp.scroll_into_view('[data-ui="apply-button"]')
     sb.sleep(5)
+    sb.cdp.save_screenshot('abc2.png', folder=None, selector=None)
     sb.cdp.find_element('[data-ui="apply-button"]', best_match=False, timeout=10).mouse_click()
+    sb.cdp.save_screenshot('abc3.png', folder=None, selector=None)
     # sb.cdp.find_element('[data-ui="apply-button"]', best_match=False, timeout=10).mouse_click()
+    sb.sleep(3)
+    sb.cdp.save_screenshot('abc4.png', folder=None, selector=None)
     # sb.sleep(10)
     sb.uc_gui_handle_cf()
     try:
@@ -24,7 +29,7 @@ with SB(uc=True, test=True, locale="en") as sb:
         # sb.cdp.find_element('[id^="turnstile-container-"] div', best_match=False, timeout=5).mouse_click()
     except Exception as e:
         print(f"Error clicking on reCAPTCHA: {e}")
-    
+    sb.cdp.save_screenshot('abc5.png', folder=None, selector=None)
     sb.sleep(10)
-    sb.cdp.save_screenshot('abc.png', folder=None, selector=None)
+    sb.cdp.save_screenshot('abc6.png', folder=None, selector=None)
     sb.cdp.find_element('[data-ui="successful-submit"]', best_match=False, timeout=10)
